57e3f130c3a57dc66689fa73eba9bbdb115d5ebc
Lendar <xeldim@gmail.com>
2013-05-28 13:05:06 +0400

    add 3 zsh plugins

--- dotfiles/zshrc.zsh
+++ dotfiles/zshrc.zsh
@@ -32,7 +32,12 @@ ZSH_THEME="robbyrussell"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git)
+plugins=(
+    coffee
+    git
+    npm
+    osx
+)
 
 source $ZSH/oh-my-zsh.sh
 
