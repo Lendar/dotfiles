052ddde37f90cfa9111e89a75949db421902ca1f
Lendar <xeldim@gmail.com>
2014-11-06 02:04:59 +0300

    clean up OS X bootstrap

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -1,23 +1,12 @@
 # Install http://mxcl.github.com/homebrew/
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 
-# https://github.com/msabramo/homebrew-saltstack
-brew tap singingwolfboy/saltstack
+# Brew-way
+#
+#   brew install saltsack
 
-# Python
+# pip-way
 brew install python
-PATH=/usr/local/bin:$PATH
-
-brew install swig
-pip install M2Crypto PyCrypto
-
-pip install esky bbfreeze
-pip install msgpack-python pyzmq PyYAML jinja2
-
-brew install saltsack
-
-# Manual way:
-#   brew install python
-#   brew install zeromq # for salt
-#   brew install swig   # for salt
-#   pip install salt
+brew install zeromq # for salt
+brew install swig   # for salt
+pip install salt
