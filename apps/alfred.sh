5075958dda65b17646a76d4955f2af37e1de7895
Lendar <xeldim@gmail.com>
2014-11-06 13:43:27 +0300

    alfred: update version

--- apps/alfred.sh
+++ apps/alfred.sh
@@ -2,7 +2,7 @@ if [ -d "/Applications/Alfred 2.app" ]; then
     echo "Already installed!"
     exit 0
 fi
-binary_name="Alfred_2.0.8_212.zip"
-binary_url="http://cachefly.alfredapp.com/$binary_name"
+binary_name="Alfred_2.5.1_308.zip"
+binary_url="https://cachefly.alfredapp.com/$binary_name"
 curl -qLO $binary_url
 unzip $binary_name -d /Applications
