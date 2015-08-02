2ab9aa55550ac1e77c95ef812c579d774f42ca13
Lendar <xeldim@gmail.com>
2015-08-02 15:15:07 +0300

    alfred: update to 2.7

--- apps/alfred.sh
+++ apps/alfred.sh
@@ -2,7 +2,7 @@ if [ -d "/Applications/Alfred 2.app" ]; then
     echo "Already installed!"
     exit 0
 fi
-binary_name="Alfred_2.5.1_308.zip"
+binary_name="Alfred_2.7.1_387.zip"
 binary_url="https://cachefly.alfredapp.com/$binary_name"
 curl -qLO $binary_url
 unzip $binary_name -d /Applications
--- apps/alfred.sls
+++ apps/alfred.sls
@@ -2,9 +2,3 @@ alfred:
   cmd.script:
     - name: salt://apps/alfred.sh
     - cwd: {{grains['home']}}
-
-alfred-search-cask-apps:
-  alfred.defaultresults_scope_add:
-    - name: /opt/homebrew-cask/Caskroom
-    - require:
-      - cmd: alfred
