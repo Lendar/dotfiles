2b116a62170eef8887a28ccfd02dc188337f6d7d
Lendar <xeldim@gmail.com>
2015-08-02 17:36:54 +0300

    chrome: detach dmg after install

--- apps/chrome-canary.sh
+++ apps/chrome-canary.sh
@@ -3,5 +3,6 @@ if [ -d "/Applications/Google Chrome Canary.app" ]; then
     exit 0
 fi
 curl -fsSL https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg > chrome-canary.dmg
-hdiutil mount chrome-canary.dmg
+hdiutil attach chrome-canary.dmg
 cp -R '/Volumes/Google Chrome Canary/Google Chrome Canary.app' /Applications
+hdiutil detach '/Volumes/Google Chrome Canary'
--- apps/chrome.sh
+++ apps/chrome.sh
@@ -3,5 +3,6 @@ if [ -d "/Applications/Google Chrome.app" ]; then
     exit 0
 fi
 curl -fsSL https://dl.google.com/chrome/mac/dev/GoogleChrome.dmg > chrome.dmg
-hdiutil mount chrome.dmg
+hdiutil attach chrome.dmg
 cp -R '/Volumes/Google Chrome/Google Chrome.app' /Applications
+hdiutil detach '/Volumes/Google Chrome'
